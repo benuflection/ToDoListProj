9315f9a56f60ce4461c31a96149dfb97e156a036
benuflection <39638302+benuflection@users.noreply.github.com>
2023-04-03 21:46:32 -0400

    Add a fabfile for automated deploys

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -1,21 +1,39 @@
 import random
 from fabric.contrib.files import append, exists
 from fabric.api import cd, env, local, run
+import logging
+import paramiko
+from paramiko import BadHostKeyException
+logging.basicConfig(level=logging.DEBUG)
+#paramiko.util.log_to_file('demo.log')
+
+look_for_keys = False
+allow_agent = False
 
 REPO_URL = 'https://github.com/benuflection/ToDoListProj'
+#hardcoded user
+
+
+env.hosts = ['mikde@raspberrypi:22']
+#env.passwords = {'mikede@raspberrypi:22': 'shittyshitterson'}
+env.passwords = {'mikede@192.168.1.9:22': 'shittyshitterson'}
 
 def deploy():
-    ####   env.user replaced with hardcoded "mikede" because user is different on testing computer and server
-    site_folder = f'/home/{env.user}/sites/{env.host}'
-    #######site_folder = f'/home/mikede/sites/{env.host}'
-    run(f'mkdir -p {site_folder}')
-    with cd(site_folder):
-        _get_latest_source()
-        _update_virtualenv()
-        _create_or_update_dotenv()
-        _update_static_files()
-        _update_database()
-
+        ####   env.user replaced with hardcoded "mikede" because user is different on testing computer and server
+    try:
+        site_folder = f'/home/{env.user}/sites/{env.host}'
+        #######site_folder = f'/home/mikede/sites/{env.host}'
+        run(f'mkdir -p {site_folder}')
+        with cd(site_folder):
+            _get_latest_source()
+            _update_virtualenv()
+            _create_or_update_dotenv()
+            _update_static_files()
+            _update_database()
+    except Exception as e:
+        print(f"Bad Host Key Exception: {e}")
+    except Exception:
+        print("Other Exception")
 def _get_latest_source():
     if exists('.git'):
         run('git fetch')
