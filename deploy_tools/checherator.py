105f68628a9f1e1cbeccc26a1bea7ab8f94c1aef
benuflection <39638302+benuflection@users.noreply.github.com>
2023-04-04 21:53:03 -0400

    First-cut FT

--- deploy_tools/checherator.py
+++ deploy_tools/checherator.py
@@ -1,1 +1,9 @@
-import time
+import random
+from fabric.contrib.files import append, exists
+from fabric.api import cd, env, local, run
+import logging
+
+#env.user = 'mikede'
+print(env.user)
+print(env.host)
+print(SITENAME)
--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -5,8 +5,35 @@ from .base import FunctionalTest
 class ItemValidationTest(FunctionalTest):
 
     def test_cannot_add_empty_list_items(self):
-        # Edith submits and empty list item, home page represhes. there is an error message
-        # She tries again with a real item which works. she tries another empty list item: another error message.
+        # Edith submits and empty list item,
 
-        self.fail('write me!')
+        self.browser.get(self.live_server_url)
+        self.browser.find_element('id', 'id_new_item').send_keys(Keys.ENTER)
+
+        # home page refreshes. there is an error message
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element('css selector', '.has-error').text,
+            "you can't have and empty list item"
+        ))
+        #She tries again with a real item which works.
+        self.browser.find_element('id', 'Id_new_item').send_keys('Buy milk')
+        self.browser.find_element('id', 'id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+
+        #  she tries another empty list item.
+        self.browser.find_element('id', 'id_new_item').send_keys(Keys.ENTER)
+
+        # Gets another error message
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element('css selector', '.has-error').text,
+            "You can't have and empty list item"
+        ))
+
+        # She can fill some text in
+        self.browser.find_element('id', 'id_new_item').send_keys('Make tea')
+        self.browser.find_element('id', 'Id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+        self.wait_for_row_in_list_table('2: Make tea')
+
+        self.fail('Finish the test!')
 #The end
