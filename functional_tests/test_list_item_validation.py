812d603d35e653e82b4bf7ed8112a2166114ec0a
benuflection <39638302+benuflection@users.noreply.github.com>
2023-04-07 13:52:22 -0400

    use form in all views, back to working state

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -11,12 +11,11 @@ class ItemValidationTest(FunctionalTest):
         self.get_item_input_box().send_keys(Keys.ENTER)
 
         # home page refreshes. there is an error message
-        self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element('css selector', '.has-error').text,
-            "You can't have an empty list item"
-        ))
+        self.wait_for(lambda: self.browser.find_element('css selector', '#id_text:invalid')
+        )
         #She tries again with a real item which works.
         self.get_item_input_box().send_keys('Buy milk')
+        self.wait_for(lambda: self.browser.find_element('css selector', '#id_text:valid'))
         self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
 
@@ -24,16 +23,16 @@ class ItemValidationTest(FunctionalTest):
         self.get_item_input_box().send_keys(Keys.ENTER)
 
         # Gets another error message
-        self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element('css selector', '.has-error').text,
-            "You can't have an empty list item"
-        ))
+        self.wait_for_row_in_list_table('1: Buy milk')
+        self.wait_for(lambda: self.browser.find_element('css selector', '#id_text:invalid')
+        )
 
         # She can fill some text in
         self.get_item_input_box().send_keys('Make tea')
+        self.wait_for(lambda: self.browser.find_element('css selector', '#id_text:valid'))
         self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
         self.wait_for_row_in_list_table('2: Make tea')
 
-        self.fail('Finish the test!')
+
 #The end
