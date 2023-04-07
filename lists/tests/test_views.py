87cccb68b6384d0b985ab4042857e6f20526a321
benuflection <39638302+benuflection@users.noreply.github.com>
2023-04-07 19:35:17 -0400

    End of 15

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -4,7 +4,10 @@ from django.test import TestCase
 #from django.template.loader import render_to_string
 from lists.models import Item, List
 from django.utils.html import escape
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.forms import (
+    DUPLICATE_ITEM_ERROR, EMPTY_ITEM_ERROR,
+    ExistingListItemForm, ItemForm,
+)
 from unittest import skip
 
 from lists.views import home_page
@@ -33,6 +36,12 @@ class ListViewTest(TestCase):
         correct_list = List.objects.create()
         response = self.client.get(f'/lists/{correct_list.id}/')
         self.assertEqual(response.context['list'], correct_list)
+
+    def test_displays_item_form(self):
+        list_ = List.objects.create()
+        response = self.client.get(f'/lists/{list_.id}/')
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
+        self.assertContains(response, 'name="text"')
 
     def test_displays_only_items_for_that_list(self):
         correct_list = List.objects.create()
@@ -107,13 +116,13 @@ class ListViewTest(TestCase):
 
     def test_for_invalid_input_passes_form_to_template(self):
         response = self.post_invalid_input()
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
 
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
 
-    @skip
+
     def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
         list1 = List.objects.create()
         item1 = Item.objects.create(list=list1, text='textey')
@@ -122,7 +131,7 @@ class ListViewTest(TestCase):
             data={'text': 'textey'}
         )
 
-        expected_error = escape("You've already got this in your list")
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
         self.assertContains(response, expected_error)
         self.assertTemplateUsed(response, 'list.html')
         self.assertEqual(Item.objects.all().count(), 1)
@@ -155,12 +164,14 @@ class NewListTest(TestCase):
         response = self.client.post('/lists/new', data={'text': ''})
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
 
-    def test_for_invalid_input_passes_form_to_template(self):
-        response = self.client.post('/lists/new', data={'text': ''})
-        self.assertIsInstance(response.context['form'], ItemForm)
+########## probably duplicated, currently only lives in ListViewTest class w/ItemForm as ExistingListItemForm
+    # def test_for_invalid_input_passes_form_to_template(self):
+    #     response = self.client.post('/lists/new', data={'text': ''})
+    #     self.assertIsInstance(response.context['form'], ItemForm)
 
-    def test_displays_item_form(self):
-        list_ = List.objects.create()
-        response = self.client.get(f'/lists/{list_.id}/')
-        self.assertIsInstance(response.context['form'], ItemForm)
-        self.assertContains(response, 'name="text"')
+# was here -- could be duplicated
+    # def test_displays_item_form(self):
+    #     list_ = List.objects.create()
+    #     response = self.client.get(f'/lists/{list_.id}/')
+    #     self.assertIsInstance(response.context['form'], ExistingListItemForm)
+    #     self.assertContains(response, 'name="text"')
